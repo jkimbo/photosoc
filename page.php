5f107d5f9caa44cfe995227b76809177c3409505
Jonathan Kim <jkimbo@gmail.com>
2011-10-01 13:42:30 +0100

    Single page layout

--- page.php
+++ page.php
@@ -1,29 +1,96 @@
 <?php get_header(); ?>
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-		<article class="post" id="post-<?php the_ID(); ?>">
+	<?php query_posts(array('showposts' => 10, 'post_parent' => 0, 'post_type' => 'page', 'orderby' => 'menu_order', 'order' => 'ASC'));
 
-			<h2><?php the_title(); ?></h2>
+	while (have_posts()) { the_post(); ?>
 
-			<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
+	<?php if ($post->ID == get_option('page_for_posts')) { // if page is news ?>
+		<section class="section blog" id="post-<?php the_ID(); ?>">
+			<article>
+				<header class="container_12">
+					<h2><?php the_title(); ?></h2>
+				</header>
 
-			<div class="entry">
+				<?php
+				$num = (int) get_category('4')->count;
+				$num_per_page = 6;
+				$num_pages = ceil($num/6);
+				$args = array( 'category' => 4, 'numberposts' => -1 );
+				$lastposts = get_posts( $args );
+				$i = 0;
+				$p = 1; ?>
 
-				<?php the_content(); ?>
+				<div class="scroll container_12">
+					<div class="scrollcont" style="width: <?php echo $num_pages*960; ?>px">
+				<?php
+				foreach($lastposts as $post) : setup_postdata($post);
+					$i++;
+					if($i == 1) echo '<div class="postcont" id="page'.$p.'">';
+					if(($i%($num_per_page+1)) == 0) {
+						$p++;
+						echo '<div class="postcont" id="page'.$p.'">';
+						echo '<div id="col1">';
+					}
 
-				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+					if(($i%($num_per_page+5)) == 0) echo '<div id="col2">';
 
+					if($i == 1) echo '<div id="col1">';
+					if($i == 4) echo '<div id="col2">';
+				?>
+
+					<a href="<?php the_permalink(); ?>" id="<?php the_ID(); ?>" class="blogpost">
+					<div class="newsBox">
+						<div class="cover">
+							<?php
+							if ( has_post_thumbnail() )
+								the_post_thumbnail();
+							else
+								the_content(); ?>
+						</div>
+						<h3><?php the_title(); ?></h3>
+					</div>
+					</a>
+
+				<?php
+					if($i == 3) echo '</div>';
+					if($i == 6) echo '</div>';
+					if(($i%$num_per_page) == 0) echo '<div class="clear"></div></div>';
+				?>
+				<?php endforeach; ?>
+					</div>
+				</div>
+			</article>
+			<div id="navigation" class="container_12">
+				<ul>
+				<li id="pagelabel">Pages:</li>
+				<?php
+					for ($d = 1; $d <= $num_pages; $d++) { ?>
+						<li id="pagelink<?php echo $d; ?>"><a href="#page<?php echo $d;?>"><?php echo $d; ?></a></li>
+				<?php  }  ?>
+				</ul>
 			</div>
+		</section>
 
-			<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+	<?php } else { ?>
+	<section class="section" id="post-<?php the_ID(); ?>">
+		<article class="container_12">
+			<header>
+				<h2><?php the_title(); ?></h2>
+			</header>
+
+			<?php if (get_option('constellation_show_subpages')=='yes') {
+				$subpages = wp_list_pages('title_li=&child_of='.$post->ID.'&echo=0&sort_column=menu_order&depth=1');
+				if ($subpages) echo '<nav id="subpages"><ul>' . str_replace('</a>',' &rarr;</a>',$subpages) . '</ul></nav>';
+			} ?>
+
+			<?php the_content(''); ?>
 
 		</article>
-		
-		<?php comments_template(); ?>
+	</section>
+	<?php } ?>
 
-		<?php endwhile; endif; ?>
-
-<?php get_sidebar(); ?>
+	<?php }
+		wp_reset_query();  // Restore global post data
+	?>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,31 +1,36 @@
 <?php get_header(); ?>
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <div id="singlepostcont" class="container_12">
+            <div class="returntomain grid_12">
+                <a href="#post-12">Back</a>
+            </div>
+            <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			
-			<h1 class="entry-title"><?php the_title(); ?></h1>
+                <h1 class="entry-title"><?php the_title(); ?></h1>
 
-			<div class="entry-content">
-				
-				<?php the_content(); ?>
+                <div class="entry-content">
 
-				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
-				
-				<?php the_tags( 'Tags: ', ', ', ''); ?>
-			
-				<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
+                    <?php the_content(); ?>
 
-			</div>
-			
-			<?php edit_post_link('Edit this entry','','.'); ?>
-			
-		</article>
+                    <?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
 
-	<?php comments_template(); ?>
+                    <?php the_tags( 'Tags: ', ', ', ''); ?>
 
+                    <?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
+
+                </div>
+
+                <?php edit_post_link('Edit this entry','','.'); ?>
+
+            </article>
+
+            <div class="returntomain grid_12">
+                <a href="#post-12">Back</a>
+            </div>
+            <div class="clear"></div>
+	<?php //comments_template(); ?>
+        </div>
 	<?php endwhile; endif; ?>
-	
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
